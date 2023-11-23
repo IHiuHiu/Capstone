7170ea7bec36f968472d8c1001f059c3c8b32bb4
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2023-11-23 11:11:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,3 +21,20 @@ def load_data():
         return index
 
 index = load_data()
+
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history

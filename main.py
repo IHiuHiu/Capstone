66c1469c88752bc8b89b9b880fd3a8a8275c3bf7
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2023-11-23 11:15:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if "messages" not in st.session_state.keys(): # Initialize the chat message hist
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
     ]
-@st.cache_resource(show_spinner=False)
+#st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
